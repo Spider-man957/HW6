7069dc5cf9c73b40d7dcf82c4df7c4e6f9e7c00a
Spider-man957 <itprogram88@gmail.com>
2025-10-24 11:09:59 +0300

    HW6

---
+++ script.js
@@ -0,0 +1,36 @@
+// let a = 1;
+// while (a <= 10) {
+//   console.log(a);
+//   a += 1;
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   } else {
+//     continue;
+//   }
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(7 * i)
+// }
+
+// n = 20;
+// for (let i = 0; i <= 100; i++) {
+//   if (i < n) {
+//     console.log(i);
+//   } else if (i >= n) {
+//     break;
+//   }
+// }
+
+// let x = 0;
+// while (x < 20) {
+//   x += 1;
+//   if (x % 3 === 0) {
+//     continue;
+//   } else {
+//     console.log(x);
+//   }
+// }
